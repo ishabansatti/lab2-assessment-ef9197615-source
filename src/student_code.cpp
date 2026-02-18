565092efd46022c130cb25935782170ff4e02dd5
ef9197615-source <ef9197615@gmail.com>
2026-02-18 18:15:47 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -8,18 +8,24 @@ int getIntValue() {
 }
 
 float getFloatValue() {
-    // Declare a variable var_float of type float and set its value to 10.75.
-    //return var_float;
+    // Declare a variable var_float of type float and set its value to 10.75.float;
+    float var_float=10.75;
+    return var_float;
+
 }
 
 char getCharValue() {
     // Declare a variable var_char of type character and set its value to A.
-    //return var_char;
+    char var_char ='A'
+    return var-char;
+     
 }
 
 int convertFloatToInt(float value) {
     // convert value to int.
-    return value;
+    int result=static_cast<int>(value);
+    return result;
+    
 }
 
 /*
@@ -29,6 +35,7 @@ Formula:
     Celsius = (Fahrenheit - 32) * 5 / 9
 */
 float fahrenheitToCelsius(float fahrenheit) {
-    //return celsius;
+    float celsius=(fahrenheit-32)*5/9;
+    return celsius;
 
 }
