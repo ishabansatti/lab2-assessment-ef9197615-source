0b03c5a2c1aca573c1895edcbe8d5cd20274f9c3
ef9197615-source <ef9197615@gmail.com>
2026-02-18 18:19:08 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -16,8 +16,8 @@ float getFloatValue() {
 
 char getCharValue() {
     // Declare a variable var_char of type character and set its value to A.
-    char var_char ='A'
-    return var-char;
+    char var_char ='A';
+    return var_char;
      
 }
 
